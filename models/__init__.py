0aa9169671485eeff952ef9bcccb96c5f85f6490
Dhairy4 <dhairyamehta025@gmail.com>
2025-12-27 13:38:20 +0530

    Delete models directory

--- models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import gearguard_team
-from . import gearguard_equipment
-from . import gearguard_request
--- models/gearguard_team.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class MaintenanceTeam(models.Model):
-    _name = 'gearguard.team'
-    _description = 'Maintenance Team'
-
-    name = fields.Char(string='Team Name', required=True)
-    member_ids = fields.Many2many('res.users', string='Members')
